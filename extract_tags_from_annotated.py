e94954c8407093febd34bff3939ed99ac5eb1e35
hundredrab <sourab.jha@kgec.edu.in>
2021-01-14 17:49:00 +0530

    format: Formatting and typos

--- extract_tags_from_annotated.py
+++ extract_tags_from_annotated.py
@@ -1,27 +1,31 @@
-import json
-import os
-import re
 import argparse
+import json
 from os import listdir
 from os.path import isfile, join
 
 
-if __name__ == '__main__':
-    ap = argparse.ArgumentParser()
-    ap.add_argument("--input", required=True, help="Input dir path")
-    ap.add_argument("--tag", required=True, help="Tag to be extracted")
-    av = ap.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", required=True, help="Input dir path")
+    parser.add_argument("--tag", required=True, help="Tag to be extracted")
+    args = parser.parse_args()
 
     documents = []
-    for pth in [join(av.input, f) for f in listdir(av.input) if isfile(join(av.input, f))]:
-        with open(pth, 'r') as json_file:
+    for pth in filter(isfile,
+                      map(lambda f: join(args.input, f), listdir(args.input))):
+
+        with open(pth, "r") as json_file:
             documents.extend([json.loads(i) for i in list(json_file)])
 
-    with open(av.tag+'s.new', 'w') as json_file:
+    with open(args.tag + "s.new", "w") as json_file:
         for document in documents:
             if document is None:
                 print("Error in")
                 continue
-            for ne in document['annotations']['named_entity']:
-                if ne['tag'].lower() == av.tag.lower():
-                    json_file.write(ne['extent'] + "\n")
+            for entity in document["annotations"]["named_entity"]:
+                if entity["tag"].lower() == args.tag.lower():
+                    json_file.write(entity["extent"] + "\n")
+
+
+if __name__ == "__main__":
+    main()
