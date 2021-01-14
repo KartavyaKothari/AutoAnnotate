0005643e5cef429999d48b24596457ac326cc9b4
Kartavya <kartavya.kothari24@gmail.com>
2021-01-14 13:07:45 +0530

    Added name annotations

---
+++ create_name_regex.py
@@ -0,0 +1,23 @@
+import json
+import os
+import re
+import argparse
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--input", required=True, help="Input file path")
+    av = ap.parse_args()
+
+    with open(av.input, 'r') as f:
+        content = f.read()
+
+    name_list = sorted(list(set(list(map(lambda x: x.lower(),content.split('\n'))))),key=len,reverse=True)
+
+    with open('outregex','w') as f:
+        outstr = []
+        for word in name_list:
+            if len(word)==0:
+                continue
+            outstr.append('\\b'+word.strip()+'\\b')
+        f.write('|'.join(outstr))
